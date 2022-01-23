b9ee2713ddfa809cf33ef4b128b4c55590cfd727
Tyler Clayton <tmc3@live.unc.ed>
2022-01-23 03:07:12 -0500

    pull

---
+++ exercises/hype_machine.py
@@ -0,0 +1,9 @@
+"""Exercise 01."""
+__author__ = "730316240"
+
+name : str = input("What is your name? ")
+print("You entered:")
+print(name)
+print(name + " still has not watched Spider-Man: Homecoming.")
+print("Don't give up on seeing it, " + name + "! ")
+print("You know what " + name + ", let's go see it tomorrow.")
