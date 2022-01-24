ffb92bb36a446f35cbdb9cb74624b3ba3aa0b524
Tyler Clayton <tmc3@live.unc.ed>
2022-01-24 15:46:05 -0500

    commit

---
+++ exercises/relational_operators.py
@@ -0,0 +1,16 @@
+"""Exercise 01."""
+
+__author__ = "730316240"
+
+Left = int(input("Pick a number."))
+Right = int(input("Pick a number."))
+
+Less = Left < Right
+Greater = Left >= Right
+Equal = Left == Right
+Unequal = Left != Right
+
+print(Left, " < "  Right  " is " Less)
+print(Left " >= " Rigtht " is " Greater)
+print(Left " == " Right " is " Equal)
+print(Left " != "  Right " is " Unequal)
